475b8808054aa8963c6302274f64922d953d4084
Makar Zotov <mzotof@ya.ru>
2021-11-22 01:19:17 +0300

    Финальная версия проекта

--- master.c
+++ master.c
@@ -1,46 +1,61 @@
-#include <inttypes.h>
 #include <avr/io.h>
-#include <avr/interrupt.h>
-#include <avr/sleep.h>
 #include <util/delay.h>
 
-void init_UART(void)
+volatile unsigned char queue[100], //очередь
+                       qlen = 0; // длина очереди
+const unsigned char digits[] = { 0b00111111,
+                                 0b00000110,
+                                 0b01011011,
+                                 0b01001111,
+                                 0b01100110,
+                                 0b01101101,
+                                 0b01111101,
+                                 0b00000111,
+                                 0b01111111,
+                                 0b01101111 }; // соответствие цифр и их обозначения на дисплее
+
+void initUART(void) // инициализация UART
 {
-    UBRRH=0;
-    UBRRL=51;
-    UCSRA=0;
-    UCSRB=0b00011000;
-    UCSRC=0b10000110;
+    UBRRL=51; //скорость 9600 Бод при f=8 МГц
+    UCSRB=(1<<RXEN)|(1<<TXEN); //разрешение приема и передачи
+    UCSRC=(1<<URSEL)|(1<<UCSZ1)|(1<<UCSZ0); //8-битные посылки
 }
 
-void send_Uart(int c)//   Отправка байта
+void sendUart(unsigned char c) // отправка по UART
 {
-    PORTD |= (1<<PD2);
+    PORTD |= (1<<PD2); // включение передачи
     UDR = c;
-    while(!(UCSRA&(1<<UDRE))) {};
-    while(!(UCSRA&(1<<TXC))) {};
-    PORTD &= ~(1<<PD2);
+    while(!(UCSRA&(1<<UDRE))) {}; // ожидание опустошения UDR
+    while(!(UCSRA&(1<<TXC))) {};  // ожидание отправки
+    PORTD &= ~(1<<PD2); // включение приема
 }
 
-volatile int queue[100], qlen = 0;
-
-int recieve_Uart(void)
+unsigned char recieveUart(void) // прием по UART
 {
-    while(!(UCSRA&(1<<RXC))) {};
+    while(!(UCSRA&(1<<RXC))) {}; // ожидание приема
+    PORTD |= (1<<PD2); // включение передачи
     return UDR;
 }
 
-void insert(int a)
+void addToQueue(unsigned char a) // добавление пользователя в очередь
 {
     queue[qlen] = a;
     qlen++;
 }
 
-int delete(void)
+void goToMK2(void) // общение с МК2
 {
-    int rett = queue[0];
-    int i = 0;
-    while (queue[i] != -1)
+    sendUart(0b10000000);
+    unsigned char data = recieveUart();
+    if (data != 0b11111111)
+        addToQueue(data&0b01111111);
+}
+
+unsigned char deleteFromQueue(void) // удаление человека из очереди
+{
+    unsigned char rett = queue[0];
+    unsigned char i = 0;
+    while (queue[i] != 0)
     {
         queue[i] = queue[i+1];
         i++;
@@ -49,91 +64,83 @@ int delete(void)
     return rett;
 }
 
+void goToMK3(void) // общение с МК3
+{
+    unsigned char i, j, data;
+
+    if (queue[0] != 0) // если очередь не пуста
+        sendUart(queue[0]); // отправка номера следующего в очереди человека
+    else
+        sendUart(0b01111111); // отправка сигнала, что очередь пуста
+
+    data = recieveUart();
+
+    if (data == 1)
+    {
+        unsigned char a = deleteFromQueue();
+        for (j = 0; j < 3; j++) // вывод вызываемого пользователя
+        {
+            for (i = 0; i < 50; i++)
+            {
+                PORTC = 0b00001010;
+                PORTA = digits[a / 10];
+                PORTB = digits[a / 10];
+                _delay_ms(10);
+                PORTC = 0b00000101;
+                PORTA = digits[a % 10];
+                PORTB = digits[a % 10];
+                _delay_ms(10);
+            } 
+            PORTC = 0b00001111;
+            _delay_ms(500);
+        }
+    }
+}
+
+void outputQueue(void) // вывод очереди
+{
+    if (queue[0] != 0)
+    {
+        if (queue[1] != 0)
+        {
+            PORTC = 0b00001010;
+            PORTA = digits[queue[0] / 10];
+            PORTB = digits[queue[1] / 10];
+        }
+        else
+        {
+            PORTC = 0b00001110;
+            PORTA = digits[queue[0] / 10];
+        }
+        _delay_ms(10);
+        if (queue[1] != 0)
+        {
+            PORTC = 0b00000101;
+            PORTA = digits[queue[0] % 10];
+            PORTB = digits[queue[1] % 10];
+        }
+        else
+        {
+            PORTC = 0b00001101;
+            PORTA = digits[queue[0] % 10];
+        }
+        _delay_ms(10);
+    }
+}
+
 int main()
 {
-    int i, j, data;
-    for (i=0;i<100;i++)
-        queue[i]=-1;
-    int digits[] = { 0b00111111,
-                     0b00000110,
-                     0b01011011,
-                     0b01001111,
-                     0b01100110,
-                     0b01101101,
-                     0b01111101,
-                     0b00000111,
-                     0b01111111,
-                     0b01101111  };
-    init_UART();
-
-    DDRA  = 0b01111111;
-    DDRB  = 0b01111111;
-    DDRC  = 0b00001111;
-    DDRD  = 0b00000110;
-    PORTD = 0b00000101;
+    initUART();
+    DDRA  = 0b01111111; // включение вывода для 1 дисплея
+    DDRB  = 0b01111111; // включение вывода для 2 дисплея
+    DDRC  = 0b00001111; // включение вывода для управляющих сигналов дисплеев
+    DDRD  = 0b00000110; // включение вывода на портах 2 (управление MAX485) и 1 (TX)
+    PORTD = 0b00000101; // включение подтягивающего резистора на порте 0 (RX) и режима отправки для MAX485
 
     while(1)
     {
-        /*send_Uart(0b10000000);
-        data = recieve_Uart();
-        PORTD |= (1<<PD2);
-        if (data != 0b11111111)
-            insert(data&0b01111111);*/
-        if (queue[0] != -1)
-            send_Uart(queue[0]);
-        else
-            send_Uart(0b01111111);
-        data = recieve_Uart();
-        /*if (data == 0)
-            _delay_ms(10000);
-        else*/ if (data == 1)
-        {
-            int a = delete();
-            qlen--;
-            for (j = 0; j < 3; j++)
-            {
-                for (i = 0; i < 50; i++)
-                {
-                    PORTC = 0b00001010;
-                    PORTA = digits[a / 10];
-                    PORTB = digits[a / 10];
-                    _delay_ms(10);
-                    PORTC = 0b00000101;
-                    PORTA = digits[a % 10];
-                    PORTB = digits[a % 10];
-                    _delay_ms(10);
-                } 
-                PORTC = 0b00001111;
-                _delay_ms(500);
-            }
-        }
-        /*if (queue[0] != -1)
-        {
-            if (queue[1] != -1)
-            {
-                PORTC = 0b00001010;
-                PORTA = digits[queue[0] / 10];
-                PORTB = digits[queue[1] / 10];
-            }
-            else
-            {
-                PORTC = 0b00001110;
-                PORTA = digits[queue[0] / 10];
-            }
-            _delay_ms(10);
-            if (queue[1] != -1)
-            {
-                PORTC = 0b00000101;
-                PORTA = digits[queue[0] % 10];
-                PORTB = digits[queue[1] % 10];
-            }
-            else
-            {
-                PORTC = 0b00001101;
-                PORTA = digits[queue[0] % 10];
-            }
-            _delay_ms(10);
-        }*/
-        _delay_ms(100);
+        goToMK2();
+        goToMK3();
+        outputQueue();
     }
 }
